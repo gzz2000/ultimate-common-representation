af0cec6794d68a0319cf3574f707a2affc21fe31
tlzmybm <tlzmybm@gmail.com>
2020-05-31 11:05:44 +0800

    visualization of convert_ratio & entropy added

--- visualization/visualize.py
+++ visualization/visualize.py
@@ -2,10 +2,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-a = np.loadtxt('similarity_UU.txt')
-a = np.flipud(a)
-t= sns.heatmap(a, vmin=0.03, vmax=0.65, cmap='magma', xticklabels=list(range(1,7)),yticklabels=list(range(6,0,-1)))
-
-t.set(xlabel='Network B',ylabel='Network A')
+a=np.loadtxt("H_1.txt")
+#a = np.loadtxt('similarity_UU.txt')
+#a = np.loadtxt('convert_ratio_UU.txt')
+#a = np.flipud(a)
+#t = sns.heatmap(a, vmin=0, vmax=0.9, cmap='PuBu', xticklabels=list(
+#    range(1, 7)), yticklabels=list(range(6, 0,-1)))
+t = sns.heatmap(a, vmin=0, vmax=10000, cmap='PuBu', xticklabels=list(
+    range(0, 10)), yticklabels=list(range(1, 7)))
+t.set(xlabel='Network B', ylabel='Network A')
 
 plt.show()
